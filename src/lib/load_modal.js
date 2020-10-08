da1a0f93f6767af9d6153e8816086fc271041c93
Matt McKeon <=>
2020-10-08 15:42:18 -0400

    Set the mode based on 'redirect' option

--- src/lib/load_modal.js
+++ src/lib/load_modal.js
@@ -12,7 +12,7 @@ export default function LoadModal(conditions = {}, state, charges) {
   const email = addEmailTestingConditions(state.username, 'creditkey.com', conditions);
   const address = new ck.Address('Test', 'User', 'Test Company', state.email_override !== '' ? state.email_override : email, '1 Test Rd', '', 'Testerville', 'CA', '11111', state.phone);
 
-  client.begin_checkout(state.cart, address, address, charges, remoteId, customerId, returnUrl, cancelUrl, 'modal')
+  client.begin_checkout(state.cart, address, address, charges, remoteId, customerId, returnUrl, cancelUrl, state.redirect ? 'redirect' : 'modal')
     .then(res => state.redirect ? window.location = res.checkout_url : ck.checkout(res.checkout_url))
     .catch(err => console.log(err));
 }
