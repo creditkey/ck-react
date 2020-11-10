4f8dad981121656830482bfa27d7f9498babd060
Matt McKeon <matt@mmckeon.com>
2020-11-09 21:50:49 -0500

    Update for changes on DevPage
    - In the component move, some updates to the dev page were lost; this
      restores those.
    - Also removed now unused App component

--- src/pages/DevPage.js
+++ src/pages/DevPage.js
@@ -15,83 +15,74 @@ import Display from "../components/Display";
 import BadButton from "../components/BadButton";
 
 const initialState = {
-  cart: [new ck.CartItem("1", "Test Product", 1000, "1-TP", 1)],
-  email_override: "",
+  cart: [new ck.CartItem('1', 'Test Product', 1000, '1-TP', 1)],
+  email_override: '',
   phone: makePhoneNumber(),
-  username: "egoodman",
+  username: process.env.REACT_APP_USERNAME
 };
 
-function HomePage() {
+function DevPage() {
   const [fico, setFico] = useState();
   const [redirect, setRedirect] = useState(false);
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
-      <h1 className="title has-text-centered" style={{ margin: "10px 0 0 0" }}>
-        <img src="ck-mark.svg" style={{ verticalAlign: "middle" }} alt="CK Logo" /> CK React
-        Test App
+      <h1 className="title has-text-centered" style={{ margin: '10px 0 0 0' }}>
+        <img alt="CreditKey" src="ck-mark.svg" style={{ verticalAlign:'middle' }} /> CK React Test App
       </h1>
 
-      <hr />
-
+      <hr/>
       <div className="container">
         <h1 className="subtitle">Override Options</h1>
         <div className="columns">
           <div className="column">
-            <Username dispatch={dispatch} username={state.username} />
+            <Username
+              dispatch={dispatch}
+              username={state.username} />
           </div>
           <div className="column">
-            <Email dispatch={dispatch} email={state.email_override} />
+            <Email
+              dispatch={dispatch}
+              email={state.email_override}
+            />
           </div>
           <div className="column">
-            <Pricing cart={state.cart} dispatch={dispatch} />
+            <Pricing
+              cart={state.cart}
+              dispatch={dispatch}
+            />
           </div>
         </div>
 
-        <hr />
+        <hr/>
 
         <h1 className="subtitle">Standard Checkout &amp; Apply Now</h1>
         <div className="columns">
-          <div className="column" style={{ borderRight: "1px solid #eeeeee" }}>
+          <div className="column" style={{ borderRight: '1px solid #eeeeee' }}>
             <div className="tabs">
               <ul>
-                <li className={!fico ? "is-active" : undefined}>
-                  <a href="?tier1" onClick={(e) => {
-                    e.preventDefault();
-                    setFico(null);
-                  }}>Tier 1</a>
-                </li>
-                <li className={fico === 651 ? "is-active" : undefined}>
-                  <a href="?tier2" onClick={(e) => {
-                    e.preventDefault();
-                    setFico(651);
-                  }}>Tier 2</a>
-                </li>
-                <li className={fico === 601 ? "is-active" : undefined}>
-                  <a href="?tier3" onClick={(e) => {
-                    e.preventDefault();
-                    setFico(601);
-                  }}>Tier 3</a>
-                </li>
+                <li className={!fico ? 'is-active' : undefined}><a onClick={() => setFico(null) }>Tier 1</a></li>
+                <li className={fico === 681 ? 'is-active' : undefined}><a onClick={() => setFico(681)}>Tier 2</a></li>
+                <li className={fico === 641 ? 'is-active' : undefined}><a onClick={() => setFico(641)}>Tier 3</a></li>
+                <li className={fico === 601 ? 'is-active' : undefined}><a onClick={() => setFico(601)}>Tier 4</a></li>
               </ul>
             </div>
 
             <p>
               <input
                 type="checkbox"
+                id="use_redirect"
                 onChange={() => setRedirect(!redirect)}
                 checked={redirect}
-                value={redirect}
-              />{" "}
-              Use Redirect
+                value={redirect} />
+              <label htmlFor="use_redirect">Use Redirect</ label>
             </p>
 
             <Display
               {...state}
               conditions={{ fico: fico }}
-              redirect={redirect}
-            />
+              redirect={redirect} />
           </div>
           <div className="column">
             <div className="has-text-weight-semibold">Apply Now</div>
@@ -99,32 +90,38 @@ function HomePage() {
               {...state}
               conditions={{ apply: true }}
               config={{
-                type: "pdp",
-              }}
-            />
-            <hr />
-            <div className="has-text-weight-semibold">
-              Alternative Apply Now
-            </div>
+                type: 'pdp'
+              }} />
+            <hr/>
+            <div className="has-text-weight-semibold">Text Apply Now</div>
             <Display
               {...state}
               conditions={{ apply: true }}
               config={{
-                type: "pdp",
-                display: "text",
-                size: "special",
-              }}
-            />
+                type: 'pdp',
+                display: 'text'
+              }} />
+            <hr/>
+            <div className="has-text-weight-semibold">Alternative Apply Now</div>
+            <Display
+              {...state}
+              conditions={{ apply: true }}
+              config={{
+                type: 'pdp',
+                display: 'text',
+                size: 'special'
+              }} />
           </div>
         </div>
 
-        <hr />
+        <hr/>
 
         <h1 className="subtitle">Pending and Decline Checkouts</h1>
         <div className="columns">
           <div className="column is-one-third">
             <BadButton
               {...state}
+              redirect={redirect}
               config={{ fico: 500 }}
               icon={faSkullCrossbones}
               label="Checkout with low FICO"
@@ -133,7 +130,8 @@ function HomePage() {
           <div className="column is-one-third">
             <BadButton
               {...state}
-              config={{ lexis: "bvi" }}
+              redirect={redirect}
+              config={{ lexis: 'bvi' }}
               icon={faPencilAlt}
               label="Checkout as Pending"
             />
@@ -141,7 +139,8 @@ function HomePage() {
           <div className="column">
             <BadButton
               {...state}
-              config={{ equifax: "frozen" }}
+              redirect={redirect}
+              config={{ equifax: 'frozen' }}
               icon={faIcicles}
               label="Checkout with Frozen Credit Report"
             />
@@ -151,7 +150,8 @@ function HomePage() {
           <div className="column is-one-third">
             <BadButton
               {...state}
-              config={{ equifax: "collections" }}
+              redirect={redirect}
+              config={{ equifax: 'collections' }}
               icon={faSkullCrossbones}
               label="Checkout with Active Collections"
             />
@@ -159,7 +159,8 @@ function HomePage() {
           <div className="column is-one-third">
             <BadButton
               {...state}
-              config={{ equifax: "revolving" }}
+              redirect={redirect}
+              config={{ equifax: 'revolving' }}
               icon={faSkullCrossbones}
               label="Checkout with Low Revolving Credit"
             />
@@ -167,7 +168,8 @@ function HomePage() {
           <div className="column">
             <BadButton
               {...state}
-              config={{ equifax: "fraud" }}
+              redirect={redirect}
+              config={{ equifax: 'fraud' }}
               icon={faSadTear}
               label="Checkout with Fraud Alert"
             />
@@ -177,7 +179,8 @@ function HomePage() {
           <div className="column is-half">
             <BadButton
               {...state}
-              config={{ equifax: "trades_and_collections" }}
+              redirect={redirect}
+              config={{ equifax: 'trades_and_collections' }}
               icon={faSkullCrossbones}
               label="Checkout with Collections and too few trades"
             />
@@ -185,7 +188,8 @@ function HomePage() {
           <div className="column is-half">
             <BadButton
               {...state}
-              config={{ fico: 500, equifax: "trades_and_collections" }}
+              redirect={redirect}
+              config={{ fico: 500, equifax: 'trades_and_collections' }}
               icon={faSkullCrossbones}
               label="Checkout with Collections and too few trades and Low FICO"
             />
@@ -196,4 +200,4 @@ function HomePage() {
   );
 }
 
-export default HomePage;
+export default DevPage;
