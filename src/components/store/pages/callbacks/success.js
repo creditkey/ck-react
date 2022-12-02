74e75d83d43e6fe09efd903097009e9499cb16f1
Camille Brown <cbrown@creditkey.com>
2022-12-02 11:20:17 -0800

    process checkout orders through demo-ecomm-api node server

--- src/components/store/pages/callbacks/success.js
+++ src/components/store/pages/callbacks/success.js
@@ -1,26 +1,55 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Page from "../../page";
+import request from "../../../../lib/request";
 
 export default () => {
   const query = new URLSearchParams(useLocation().search);
   const creditKeyId = query.get("id");
+  const [loading, setLoading] = useState(false);
   const [checkoutStatus, setCheckoutStatus] = useState(undefined);
 
   useEffect(() => {
     // complete order with backend
-    fetch(`https://c2rxvcaph7.execute-api.us-west-2.amazonaws.com/staging/demo-success?ckkey=${creditKeyId}`)
-      .then((res) => res.json())
-      .then((json) => {
-        setCheckoutStatus(json);
-      });
+    if (creditKeyId) {
+      setLoading(true);
+      request(`${ process.env.REACT_APP_BACKEND_URL }${ creditKeyId }`, {})
+        .then((res) => {
+          if (res) {
+            setCheckoutStatus({ status: 200, ...res });
+            setLoading(false);
+          }
+        })
+        .catch(err => {
+          setCheckoutStatus(err);
+          setLoading(false);
+        })
+    }
   }, [creditKeyId]);
+
+  if (loading) {
+    return (
+      <div className="p-6 is-flex is-flex-direction-column is-justify-content-center is-align-items-center" style={{ minHeight: '70vh' }}>
+        <FontAwesomeIcon icon={faSpinner} spin className="is-size-2"/>
+        <h3 className="title is-3 my-4">Order Processing</h3>
+      </div>
+    );
+  }
 
   return (
     <Page>
       <div className="columns">
 
         <div className="column is-7">
+          {checkoutStatus && checkoutStatus?.status !== 200 &&
+            <article className="message is-danger">
+              <div className="message-body">
+                An error occurred. Please contact Credit Key technical support for further assistance.
+              </div>
+            </article>
+          }
           <div className="thank-you">
             
               <img className="blue-checkmark" src="/images/blue_checkmark.png" alt="Blue Checkmark"/>
